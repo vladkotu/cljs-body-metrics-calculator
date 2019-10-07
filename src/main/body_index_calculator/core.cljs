922ea7679af6f6e927a0a4df9be4f80fdb60555c
Vlad Kotushenko <volodymyr_kotushenko@.epam.com>
2019-10-07 12:08:47 +0300

    convert value types to make sure store contains only valid stuff
    genre should be keyword
    age weight and height should be numbers

--- src/main/body_index_calculator/core.cljs
+++ src/main/body_index_calculator/core.cljs
@@ -8,25 +8,39 @@
  :init
  (fn [_ _]
    {:form {:gender {:visited? false
-                    :value (second (:genres config))}
+                    :value    (second (:genres config))}
            :age    {:visited? false
-                    :value (:default-age config)}
+                    :value    (:default-age config)}
            :weight {:visited? false
-                    :value ""}
+                    :value    80}
            :height {:visited? false
-                    :value ""}}}))
+                    :value    180}}}))
 
-(rf/reg-event-db :gender (fn [db [_ gender]] (assoc-in db [:form :gender]
-                                                       (update gender :value keyword))))
+(defn value->int [a-map]
+  (update a-map
+          :value
+          #(-> % (clojure.string/replace #"[^0-9]+" "") (js/parseInt 10))))
+
+(defn value->keyword [a-map]
+  (update a-map :value keyword))
+
+(defn value->string [a-map]
+  (update a-map :value #(if (keyword? %) (name %) (str %))))
+
+(rf/reg-event-db :gender (fn [db [_ gender]] (assoc-in db [:form :gender] (value->keyword gender))))
 (rf/reg-event-db :age    (fn [db [_ age]]    (assoc-in db [:form :age] age)))
-(rf/reg-event-db :weight (fn [db [_ weight]] (assoc-in db [:form :weight] weight)))
-(rf/reg-event-db :height (fn [db [_ height]] (assoc-in db [:form :height] height)))
+(rf/reg-event-db :weight (fn [db [_ weight]] (assoc-in db [:form :weight] (value->int weight))))
+(rf/reg-event-db :height (fn [db [_ height]] (assoc-in db [:form :height] (value->int height))))
 
-(rf/reg-sub :form               (fn [db _] (:form db)))
-(rf/reg-sub :gender :<- [:form] (fn [db _] (:gender db)))
-(rf/reg-sub :age    :<- [:form] (fn [db _] (:age db)))
-(rf/reg-sub :weight :<- [:form] (fn [db _] (:weight db)))
-(rf/reg-sub :height :<- [:form] (fn [db _] (:height db)))
+(rf/reg-sub :form                 (fn [db _] (:form db)))
+(rf/reg-sub :gender   :<- [:form] (fn [db _] (value->string (:gender db))))
+(rf/reg-sub :age      :<- [:form] (fn [db _] (:age db)))
+(rf/reg-sub :weight   :<- [:form] (fn [db _] (value->string (:weight db))))
+(rf/reg-sub :height   :<- [:form] (fn [db _] (value->string (:height db))))
+(rf/reg-sub :visited? :<- [:form] (fn [db _] (map (comp :visited? second) db)))
+
+(let [vs @(rf/subscribe [:visited?])]
+  (prn vs))
 
 (defn render []
   (r/render [app] (js/document.getElementById "core")))
