7e753c5306607cafe8fdcd4346bb80007a97c5ff
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-10-12 10:17:42 +0300

    print out rendering exception in a more readable way

--- src/main/body_index_calculator/core.cljs
+++ src/main/body_index_calculator/core.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [body-index-calculator.events]
+            [clojure.string :refer [split]]
             [body-index-calculator.subscriptions]
             [body-index-calculator.events :as e]
             [body-index-calculator.components.app :refer [app]]
@@ -14,8 +15,9 @@
       (fn [e] (reset! error e) #js {:error true})
       :component-did-catch
       (fn [_ error _]
-        (for [row (clojure.string/split (.-message error) "\n")]
-          (prn row)))
+        (prn :component-did-catch)
+        (doseq [r (split (str (.-message error)) "\n" )]
+          (prn r)))
       :reagent-render
       (fn [] (if @error
                [:div "Something went wrong."
@@ -31,6 +33,7 @@
     (prn (str (count done) " functions found and instrumented"))))
 
 (defn on-hmr-reload []
+  (prn :on-hmr-reload)
   (rf/clear-subscription-cache!)
   (prn "re-frame cache clean")
   (instrument-specs)
@@ -40,6 +43,5 @@
   (prn "start initialisation")
   (rf/dispatch-sync [::e/init])
   (prn "db initialisation finished")
-  (render))
-
-(instrument-specs)
+  (render)
+  (instrument-specs))
