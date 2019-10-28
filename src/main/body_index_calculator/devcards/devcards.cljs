144da0f4e15d10cb2bb7baec0ea135e98f05e641
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-10-28 11:28:41 +0200

    move devcards to separate folder

--- src/main/body_index_calculator/devcards/devcards.cljs
+++ src/main/body_index_calculator/devcards/devcards.cljs
@@ -1,8 +1,7 @@
-(ns body-index-calculator.devcards
+(ns body-index-calculator.devcards.devcards
   (:require [body-index-calculator.devcards.reagent-examples]
             [body-index-calculator.devcards.material-grids]
             [devcards.core :as dc]))
-
 
 (defn main []
   (dc/start-devcard-ui!))
