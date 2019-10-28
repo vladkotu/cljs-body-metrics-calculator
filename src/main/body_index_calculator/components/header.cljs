b87a83b0be96062c8f8a559e79090384430c6109
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-10-28 21:17:21 +0200

    complete side menu model

--- src/main/body_index_calculator/components/header.cljs
+++ src/main/body_index_calculator/components/header.cljs
@@ -1,18 +1,22 @@
 (ns body-index-calculator.components.header
   (:require
-   [body-index-calculator.components.menu-button :refer [menu-button]]
+   [body-index-calculator.components.side-menu :refer [menu-button side-menu]]
    ["@material-ui/core/AppBar" :default AppBar]
    ["@material-ui/core/Grid" :default Grid]
    ["@material-ui/core/Toolbar" :default Toolbar]
-   ["@material-ui/core/Typography" :default Typography]))
+   ["@material-ui/core/Typography" :default Typography]
+   [reagent.core :as r]))
 
 (defn header-content []
-  [:> AppBar {:position "static"
-              :style    {:background "#006064"}}
-   [:> Toolbar
-    [menu-button {:on-click #(js/console.log "menu button")}]
-    [:> Typography {:variant "h5" :component "h1"}
-     "Body Indexes Calculator"]]])
+  (r/with-let [open? (r/atom false)]
+    [:> AppBar {:position "static"
+                :style    {:background "#006064"}}
+     [:> Toolbar
+      [side-menu {:open @open? :on-close #(swap! open? not)}]
+      [menu-button {:on-click #(swap! open? not)}]
+
+      [:> Typography {:variant "h5" :component "h1"}
+       "Body Indexes Calculator"]]]))
 
 (defn header []
   [:> Grid {:item true} [header-content]])
--- src/main/body_index_calculator/components/side_menu.cljs
+++ src/main/body_index_calculator/components/side_menu.cljs
@@ -6,7 +6,6 @@
    ["@material-ui/icons/Language" :default LanguageIcon]
    ["@material-ui/icons/ColorLens" :default ColorLensIcon]
    ["@material-ui/icons/Straighten" :default StraightenIcon]
-   ["@material-ui/core/Button" :default Button]
    ["@material-ui/core/Divider" :default Divider]
    ["@material-ui/core/Box" :default Box]
    ["@material-ui/core/Drawer" :default Drawer]
@@ -16,6 +15,9 @@
    ["@material-ui/core/ListItem" :default ListItem]
    ["@material-ui/core/ListItemText" :default ListItemText]
    ["@material-ui/core/ListItemIcon" :default ListItemIcon]
+
+   ["@material-ui/core/ButtonGroup" :default ButtonGroup]
+   ["@material-ui/core/Button" :default Button]
    ["@material-ui/core/IconButton" :default IconButton]))
 
 (defn menu-button [{:keys [on-click]}]
@@ -30,33 +32,49 @@
     [:> Typography  {:variant "h5"} "Settings"]]
    [:> Divider]])
 
+(defn button-picker [props]
+  [:> ButtonGroup {:size "small"}
+   (for [item (:items props)]
+     [(r/adapt-react-class Button)
+      {:variant (if (:active? item) "contained" "outlined")
+       :key (:label item)}
+      (:label item)])])
+
 (defn language-picker []
-  [:span "lang picker"])
+  [button-picker
+   {:items [{:label "Eng" :active? true}
+            {:label "Ru"  :active? false}]}])
 
 (defn units-picker []
-  [:span "units picker"])
+  [button-picker
+   {:items [{:label "Metric" :active? true}
+            {:label "Imperial" :active? false}]}])
 
 (defn theme-picker []
-  [:span "theme picker"])
+  [button-picker
+   {:items [{:label "Light" :active? true}
+            {:label "Dark" :active? false}]}])
 
 (defn side-menu [{:keys [open on-close]}]
   [:> Drawer {:open     open
               :on-close on-close}
    [:> Box {:pt    (spacing (/ 1 4))
-            :style {:width "auto"}}
-    [:> List {:subheader (r/as-element (menu-header))
-              :style     {:width (spacing 40)}}
-     (for [item [{:label    "Pick your language"
-                  :icon     LanguageIcon
-                  :controls (r/as-element [:> Button "button"])}
-                 {:label    "System Units"
-                  :icon     StraightenIcon
-                  :controls (r/as-element [:> Button "button"])}
-                 {:label    "Theme"
-                  :icon     ColorLensIcon
-                  :controls (r/as-element [:> Button "button"])}]]
-       ^{:key (:label item)}
-       [:> ListItem {:align-items "flex-start"}
-        [:> ListItemIcon [:> (:icon item) {:font-size "large"}]]
-        [:> ListItemText {:primary   (:label item)
-                          :secondary (:controls item)}]])]]])
+            :style {:width (spacing 40)}}
+    [menu-header]
+    (for [item [{:label    "Pick your language"
+                 :icon     LanguageIcon
+                 :controls [language-picker]}
+                {:label    "System Units"
+                 :icon     StraightenIcon
+                 :controls [units-picker]}
+                {:label    "Theme"
+                 :icon     ColorLensIcon
+                 :controls [theme-picker]}]]
+      ^{:key (:label item)}
+      [:> Box {:m 3 :display "flex" :flex-direction "row"}
+       [:> Box {:mr 2 :color "text.secondary"}
+        [:> (:icon item) {:font-size "large"}]]
+       [:> Box {:display "flex" :flex-direction "column"}
+        [:> Typography {:variant "h6" :element "div"}
+         (:label item)]
+        [:> Box {:mt 1} [r/as-element (:controls item)]]]])]])
