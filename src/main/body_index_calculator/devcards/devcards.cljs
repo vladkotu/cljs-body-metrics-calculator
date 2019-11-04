ca521d88af1ac1d48023d41a230c0adb0e6399fe
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-11-04 17:05:40 +0200

    devcards broken cause of components have rf state binding in

--- src/main/body_index_calculator/devcards/devcards.cljs
+++ src/main/body_index_calculator/devcards/devcards.cljs
@@ -31,72 +31,72 @@
   (fn [_ _]
     [with-theme {:theme "light"}
      [:> Box {:m (spacing 1)}
-      [language-picker]]]))
+      [:span "language picker was here"]]]))
 
-(defcard-rg units-picker-card ""
-  (fn [_ _]
-    [with-theme {:theme "light"}
-     [:> Box {:m (spacing 1)}
-      [units-picker]]]))
+;; (defcard-rg units-picker-card ""
+;;   (fn [_ _]
+;;     [with-theme {:theme "light"}
+;;      [:> Box {:m (spacing 1)}
+;;       [units-picker]]]))
 
-(defcard-rg theme-picker-card ""
-  (fn [_ _]
-    [with-theme {:theme "light"}
-     [:> Box {:m (spacing 1)}
-      [theme-picker]]]))
+;; (defcard-rg theme-picker-card ""
+;;   (fn [_ _]
+;;     [with-theme {:theme "light"}
+;;      [:> Box {:m (spacing 1)}
+;;       [theme-picker]]]))
 
-(defcard-rg setings-menu ""
-  (fn [db _]
-    [with-theme {:theme "light"}
-     [side-menu {:open     (:side-menu @db)
-                 :on-close #(swap! db update :side-menu not)}]
-     [:> Box {:m (spacing 1)}
-      [menu-button {:on-click #(swap! db update :side-menu not)}]]])
+;; #_(defcard-rg setings-menu ""
+;;   (fn [db _]
+;;     [with-theme {:theme "light"}
+;;      [side-menu {:open     (:side-menu @db)
+;;                  :on-close #(swap! db update :side-menu not)}]
+;;      [:> Box {:m (spacing 1)}
+;;       [menu-button {:on-click #(swap! db update :side-menu not)}]]])
 
-  (r/atom {:side-menu false})
-  {:inspect-data true})
+;;   (r/atom {:side-menu false})
+;;   {:inspect-data true})
 
-(defcard-rg ready-not-ready-icons ""
-  (fn [done _]
-    [with-theme {:theme "light"}
-     [:> Box {:m (spacing (/ 1 4))}
-      [:> Grid {:container true :direction "column" :spacing 3}
-       [:> Grid {:item true :xs 6}
-        [:button {:on-click #(swap! done not)
-                  :style {:width (spacing 10)}}
-         (if @done "Undone" "Done")]]
-       [:> Grid {:item true :xs 6}
-        [:> Grid {:container true :direction "row" :spacing 2}
-         [:> Grid {:item true :xs 3}
-          [status-icon {:done? @done
-                        :label "BMI"
-                        :help-text "What is BMI"}]]
-         [:> Grid {:item true :xs 3}
-          [status-icon {:done? (not @done)
-                        :label "LBM"
-                        :help-text "What is LBM"}]]]]]]])
-  (r/atom true)
-  {:inspect-data true})
+;; (defcard-rg ready-not-ready-icons ""
+;;   (fn [done _]
+;;     [with-theme {:theme "light"}
+;;      [:> Box {:m (spacing (/ 1 4))}
+;;       [:> Grid {:container true :direction "column" :spacing 3}
+;;        [:> Grid {:item true :xs 6}
+;;         [:button {:on-click #(swap! done not)
+;;                   :style {:width (spacing 10)}}
+;;          (if @done "Undone" "Done")]]
+;;        [:> Grid {:item true :xs 6}
+;;         [:> Grid {:container true :direction "row" :spacing 2}
+;;          [:> Grid {:item true :xs 3}
+;;           [status-icon {:done? @done
+;;                         :label "BMI"
+;;                         :help-text "What is BMI"}]]
+;;          [:> Grid {:item true :xs 3}
+;;           [status-icon {:done? (not @done)
+;;                         :label "LBM"
+;;                         :help-text "What is LBM"}]]]]]]])
+;;   (r/atom true)
+;;   {:inspect-data true})
 
-(defcard-rg icons-in-a-grid-grid
-  (fn [_ _]
-    [with-theme {:theme "light"}
-     [:> Box {:m (spacing (/ 1 4))}
-      [:> Grid {:container true
-                :justify "center"
-                :spacing 3}
-       [:> Grid {:item true
-                 :xs 12}
-        [:> Paper
-         [:> Grid {:container true
-                   :justify "space-around"
-                   :spacing 3}
-          (for [x [{:done? true :label "BMI"}
-                   {:done? false :label "BMR"}
-                   {:done? false :label "LBM?"}]]
-            ^{:key (str "name-" (:label x))}
-            [:> Grid {:item true :xs 4}
-             [status-icon x]])]]]]]]))
+;; (defcard-rg icons-in-a-grid-grid
+;;   (fn [_ _]
+;;     [with-theme {:theme "light"}
+;;      [:> Box {:m (spacing (/ 1 4))}
+;;       [:> Grid {:container true
+;;                 :justify "center"
+;;                 :spacing 3}
+;;        [:> Grid {:item true
+;;                  :xs 12}
+;;         [:> Paper
+;;          [:> Grid {:container true
+;;                    :justify "space-around"
+;;                    :spacing 3}
+;;           (for [x [{:done? true :label "BMI"}
+;;                    {:done? false :label "BMR"}
+;;                    {:done? false :label "LBM?"}]]
+;;             ^{:key (str "name-" (:label x))}
+;;             [:> Grid {:item true :xs 4}
+;;              [status-icon x]])]]]]]]))
 
 (defn main []
   (dc/start-devcard-ui!))
