87a5f149216956392f34b0c431802d042d957c31
vladkotu <389119+vladkotu@users.noreply.github.com>
2023-10-04 13:40:45 +0000

    Deploying to gh-pages from @ vladkotu/cljs-body-metrics-calculator@9be1ad86a694bdcb4790ab4dfc612ad504517e07 🚀

--- js/cljs-runtime/orchestra.core.js
+++
@@ -1,5 +0,0 @@
-goog.provide('orchestra.core');
-goog.require('cljs.core');
-goog.require('orchestra.detail');
-
-//# sourceMappingURL=orchestra.core.js.map
--- js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,3 +0,0 @@
-
-body_index_calculator.core.init();
-shadow.cljs.devtools.client.browser.module_loaded('main');
